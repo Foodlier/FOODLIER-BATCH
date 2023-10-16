71d68bd62b870d1d1f1794c7ba96cad3b697c2ed
J-HyeonSeo <jcs2349@gmail.com>
2023-10-16 21:20:39 +0900

    Initial Commit

---
+++ mysqlbatch/src/main/java/com/zerobase/MySQLBatchApplication.java
@@ -0,0 +1,11 @@
+package com.zerobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySQLBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySQLBatchApplication.class, args);
+    }
+}
